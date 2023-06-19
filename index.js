a9f66ef0f27dc9db2605b78f0d7d6144263721db
Jaga_Natarajan <120781427+JagaNatarajan@users.noreply.github.com>
2023-06-19 23:50:11 +0530

    Add files via upload
    HOISTING?
    Nothing but a simple concept , hoisting is the default behavior of moving all the declaration top of the scope before code execution.

---
+++ index.js
@@ -0,0 +1,10 @@
+//function declaration
+function addNoDeclaration(){
+    console.log('Adding no from Func.declaration');
+}
+
+//function expression
+let addNoExpression = function () {
+    console.log('Adding no from Func. expression');
+}
+addNoExpression();
